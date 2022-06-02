3f398e1ac49ea588db390dc479a99e7498cb13b5
Tom Gregor <tgregor@ivsoftware.com>
2022-06-02 13:03:15 -0600

    Add project files.

---
+++ custom_back_button/custom_back_button/HeaderNavigation.xaml.cs
@@ -0,0 +1,16 @@
+﻿// https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/images?tabs=macos
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace custom_back_button
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderNavigation : StackLayout
+    {
+        public HeaderNavigation()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ custom_back_button/custom_back_button/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace custom_back_button
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainPageBinding();
+            InitializeComponent();
+        }
+    }
+    class MainPageBinding
+    {
+        public MainPageBinding()
+        {
+            BackButtonPressedCommand = new Command(OnBackButtonPressed);
+        }
+
+        public ICommand BackButtonPressedCommand { get; private set; }
+        private void OnBackButtonPressed(object o)
+        {
+            App.Current.MainPage.DisplayAlert("Message from button", "It worked", "OK");
+        }
+    }
+}
